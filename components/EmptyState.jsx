c17793794ae58f629c19bf08074e239fea1889e1
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2024-10-24 20:27:55 +0530

    emptystate button fix

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -1,33 +1,38 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { images } from '../constants'
-import CustomButton from './CustomButton'
-import { router } from 'expo-router'
+// EmptyState.js
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { images } from '../constants';
+import CustomButton from './CustomButton';
+import { router } from 'expo-router';
 
-const EmptyState = ({title,subtitle}) => {
+const EmptyState = ({ title, subtitle, buttonTitle, onButtonPress }) => {
   return (
     <View className="items-center justify-center px-4">
       <Image  
         source={images.empty}
         className="w-[270px] h-[215px]"
         resizeMode="contain"
-       />
-        <Text className="font-psemibold mt-2 text-center text-xl text-white">
-            {title}
-        </Text>
-        <Text className="font-pmedium text-sm text-gray-100">
-            {subtitle}
-        </Text>
+      />
+      <Text className="font-psemibold mt-2 text-center text-xl text-white">
+          {title}
+      </Text>
+      <Text className="font-pmedium text-sm text-gray-100">
+          {subtitle}
+      </Text>
 
-        <CustomButton 
-          title="Create Video"
-          handlePress={() => router.push('/create')}
-          containerStyles="w-full mt-4"
-
-        />
-        
+      <CustomButton 
+        title={buttonTitle || "Create Video"} // Default button title
+        handlePress={onButtonPress || (
+          () => router.push('/create'))} // Default no-op function
+        containerStyles="w-full mt-4"
+      />
     </View>
-  )
+  );
 }
 
-export default EmptyState
+export default EmptyState;
+
+
+
+
+
